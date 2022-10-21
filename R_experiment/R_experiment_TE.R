843c726585366bcddf6c80a4a1898487f9b134d6
Vadim Arzamasov <arzamasov@i40pool.ipd.kit.edu>
2022-10-21 13:45:22 +0200

    corrected the experiment

--- R_experiment/R_experiment_TE.R
+++ R_experiment/R_experiment_TE.R
@@ -1,11 +1,14 @@
 library(rpart)         # for learning DT
 library(rpart.plot)    # for plotting DT
+library(OpenML)
 
-#### Encoders
+
+#### MTE encoder
 
 # Mean Tearget Encoder (as a preprocessing step)
-# d - dataset to learn and apply TE
-# cols - a vector of names of columns to encode
+# d --- dataset to learn and apply TE
+# cols --- a vector of names of columns to encode
+
 encode_te <- function(d, cols){
   require(dataPreparation)  # contains TE implementation
   colsorig <- colnames(d)   # save names of columns
@@ -13,54 +16,72 @@ encode_te <- function(d, cols){
   # Construct encoding
   te <- build_target_encoding(d, cols_to_encode = cols,
                       target_col = "target", functions = c("mean"))
-  # apply encoding
-  d <- data.frame(target_encode(d, target_encoding = te))
+  
+  d <- data.frame(target_encode(d, target_encoding = te))   # apply encoding
   d <- d[, !(names(d) %in% cols)]   # drop original categorical columns, retain encoded 
   colsnew <- colnames(d)
   
-  # rename columns as it was originally
-  colnames(d) <- gsub("target_mean_by_", "", colsnew)
+  colnames(d) <- gsub("target_mean_by_", "", colsnew)   # rename columns as it was originall
   d <- d[, colsorig]   # make sure there is no extra columns
-  # so 'rpart' will recognize, it is classification, not regression task:
-  d$target <- as.factor(d$target)
   return(d)
 }
 
+#### factor encoding
 
-# Factor Encoder - ensures that columns to encode have 
-# 'factor' data type. 'rpart' function recognizes 
-# categorical data with this type and treats it respectively
-# d - dataset to learn and apply TE
-# cols - a vector of names of columns to encode
-encode_factor <- function(d, cols){
+function(d, cols){
   for (i in cols){
     d[, i] <- as.factor(d[, i])
   }
-  # so 'rpart' will recognize, it is classification, not regression task:
-  d$target <- as.factor(d$target)
   return(d)
 }
 
 
-#### Load Data
+#### fill NAs
 
-d <- read.csv('kick.csv')   # Use a separate script to download data
-d$target <- ifelse(d$target == "False", 0, 1)
+fill.na <- function(d){
+  colsc <- colnames(d)[grepl('cat', colnames(d), fixed = TRUE)]   # names of categorical columns
+  colsn <- colnames(d)[grepl('num', colnames(d), fixed = TRUE)]   # names of numeric columns
+  
+  for(i in colsc){
+    d[, i] <- addNA(d[, i])
+  }
+  
+  for(i in colsn){
+    d[is.na(d[, i]), i] <- mean(d[, i], na.rm = TRUE)
+  }
+  
+  return(d)
+}
+
+
+#### kick dataset
+
+d <- getOMLDataSet(41162)[['data']]
+colnames(d)[which(colnames(d) == 'IsBadBuy')] <- 'target'
+d$target <- ifelse(d$target == "0", 0, 1)
+
+catcols <- c("PurchDate", "Auction", "Make", "Model", "Trim", "SubModel", "Color", "Transmission", 
+             "WheelTypeID", "WheelType", "Nationality", "Size", "TopThreeAmericanName", 
+             "PRIMEUNIT", "AUCGUART", "BYRNO", "VNZIP1", "VNST", "IsOnlineSale")
+
+colnames(d)[!colnames(d) %in% c(catcols, 'target')] <- paste0('num_', 1:(ncol(d) - length(catcols) - 1))
+colnames(d)[colnames(d) %in% catcols] <- paste0('cat_', 1:length(catcols))
+
+d <- fill.na(d)
 cols <- colnames(d)[grepl('cat', colnames(d), fixed = TRUE)]   # names of categorical columns
+
 
 #### Experiment
 
-# MTE as preprocessing
-d_te <- encode_te(d, cols)
-# converting to 'factor' type (recursive MTE)
-d_f <- encode_factor(data.frame(d), cols)
-# converting to 'factor' type via MTE (lossy)
-d_tef <- encode_factor(data.frame(d_te), cols)
+d_te <- encode_te(d, cols) # MTE as preprocessing
+dt_te <- rpart(target ~ ., d_te, method = "class")   # MTE as preprocessing
+rpart.plot(dt_te)
 
-# train models
-dt_te <- rpart(target ~ ., d_te)   # MTE as preprocessing
-dt_f <- rpart(target ~ ., d_f)     # recursive MTE
-dt_tef <- rpart(target ~ ., d_tef) # lossy recursive MTE
+d_f <- d
+d_f$target <- as.factor(d_f$target)
+dt_f <- rpart(target ~ ., d_f, method = "class")     # recursive MTE
+rpart.plot(dt_f)
+
 
 # plot models
 jpeg(height = 350, width = 350, "dt_te.jpg")
@@ -71,16 +92,19 @@ jpeg(height = 350, width = 350, "dt_f.jpg")
 plot(dt_f)
 dev.off()
 
-jpeg(height = 350, width = 350, "dt_tef.jpg")
-plot(dt_tef)
-dev.off()
 
-# TODO: make the plots prettier! (drop split conditions)
-# rpart.plot(dt_te)
-# rpart.plot(dt_f)
+# d_tef <- encode_factor(data.frame(d_te), cols) # converting to 'factor' type via MTE (lossy)
+# dt_tef <- rpart(target ~ ., d_tef, method = "class") # lossy recursive MTE
 # rpart.plot(dt_tef)
+
+# jpeg(height = 350, width = 350, "dt_tef.jpg")
+# plot(dt_tef)
+# dev.off()
+
+# # TODO: make the plots prettier! (drop split conditions)
 # 
-# print(dt_te)
-# print(dt_f)
-# print(dt_tef)
+# # 
+# # print(dt_te)
+# # print(dt_f)
+# # print(dt_tef)
 
--- R_experiment/data_preparation.R
+++
@@ -1,5 +0,0 @@
-unzip("training.csv.zip")
-unzip("test.csv.zip")
-train <- read.csv("training.csv")
-test <- read.csv("test.csv")
-
