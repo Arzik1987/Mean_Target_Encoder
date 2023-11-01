d643a8b54aea39fa3ebe0e7aa4d2d9274b612a7f
Vadim Arzamasov <arzamasov@i40pool.ipd.kit.edu>
2023-11-01 21:33:03 +0100

    simpler experiment code

--- mte_recursive/mte_vs_factor_experiment.R
+++ mte_recursive/mte_vs_factor_experiment.R
@@ -1,110 +1,42 @@
-library(modelr)
-library(purrr)
-library(dplyr)
 library(rpart)
 source('../data/get_data.R')
-
-
-####
-
-get_pred  <- function(model, test_data){
-  data  <- as.data.frame(test_data)
-  pred  <- add_predictions(data, model)
-  return(pred)
-}
-
-get_pred2  <- function(model, test_data){
-  data  <- as.data.frame(test_data)
-  pred  <- add_predictions(data, model, type = 'vector')
-  return(pred)
-}
-
-encode_te <- function(dtr, dte){
-  require(dataPreparation)                                                      # contains TE implementation
-  colnames(dtr) <- gsub(".*\\.","",colnames(dtr))
-  colnames(dte) <- gsub(".*\\.","",colnames(dte))
-  
-  colsorig <- colnames(dtr)                                                     # save names of columns
-  cols <- colnames(dtr)[grepl('cat', colnames(dtr), fixed = TRUE)]              # names of categorical columns
-  target = colnames(dtr)[grepl('target', colnames(dtr), fixed = TRUE)]
-  
-  te <- build_target_encoding(dtr, cols_to_encode = cols,
-                              target_col = target, functions = c("mean"))       # Construct encoding
-  
-  dtr <- data.frame(target_encode(dtr, target_encoding = te))                   # apply encoding train
-  dte <- data.frame(target_encode(dte, target_encoding = te))                   # apply encoding test
-  
-  dte <- dte[, !(names(dte) %in% cols)]                                         # drop original categorical columns, retain encoded 
-  colnames(dte) <- gsub("target_mean_by_", "", colnames(dte))                   # rename columns as it was originally
-  dte <- dte[, colsorig]                                                        # make sure there is no extra columns
-  
-  dtr <- dtr[, !(names(dtr) %in% cols)]                                         # drop original categorical columns, retain encoded 
-  colnames(dtr) <- gsub("target_mean_by_", "", colnames(dtr))                   # rename columns as it was originally
-  dtr <- dtr[, colsorig]                                                        # make sure there is no extra columns
-  return(list(dtr, dte))
-}
-
+source('../utils/encode_mte.R')
 
 ####
 
-experiment <- function(dsize = -1, controls){
+test_mte_factor <- function(dname, controls){
   
-  accuracy <- list()
+  accuracyf <- numeric()
+  accuracymte <- numeric()
+
+  set.seed(42)
+  print(dname)
+
+  d <- get.data(dname)
+  d <- fill.na(d) 
+  trind <- sample.int(nrow(d), floor(nrow(d)*0.7))
+  d <- list(d[trind,], d[-trind,])
+  dmte = encode_te(d[[1]], d[[2]])
+      
   for(j in 1:length(controls)){
-    accuracy[[j]] <- tibble()
+    model = rpart(target ~ ., data = d[[1]], method = "class", control = controls[[j]])
+    pred <- predict(model, d[[2]], type = 'vector')
+    acuracyf <- c(accuracyf, mean(d[[2]]$target == (pred - 1)))
+    
+    model = rpart(target ~ ., data = dmte[[1]], method = "class", control = controls[[j]])
+    pred <- predict(model, dmte[[2]], type = 'vector')
+    acuracymte <- c(accuracymte, mean(dmte[[2]]$target == (pred - 1)))
   }
-  dnames <- c('kick', 'upselling', 'internet', 'churn', 'appetency', 
-              'epsilon', 'click', 'amazon', 'adult')
-  
-  for(dname in dnames){
-    
-    print(dname)
-    set.seed(dsize)
-    if(dsize <= 0){
-      cv <- crossv_kfold(get.data(dname), k = 5)
-    } else {
-      cv <- crossv_kfold(sample_n(get.data(dname), dsize), k = 5)
-    }
-    
-    tmpacc <- list()
-    for(j in 1:length(controls)){
-      models <- map(cv$train, ~rpart(target ~ ., data = ., method = "class", control = controls[[j]]))
-      
-      pred  <- map2_df(models, cv$test, get_pred2, .id = "Run")
-      tmpacc[[j]] <- pred %>% group_by(Run) %>% summarise(fe = mean(target == (pred - 1)))
-      tmpacc[[j]]$dname = dname
-      tmpacc[[j]]$te = NA
-    }
-    
-    for(i in 1:nrow(cv)){
-      print(i)
-      tmp = encode_te(as.data.frame(cv$train[i]), as.data.frame(cv$test[i]))
-      
-      for(j in 1:length(controls)){
-        model = rpart(target ~ ., data = tmp[[1]], method = "class", control = controls[[j]])
-        
-        predte <- predict(model, tmp[[2]], type = 'vector')
-        tmpacc[[j]]$te[i] = mean(tmp[[2]]$target == (predte - 1))
-      }
-    }
-    
-    for(j in 1:length(controls)){
-      accuracy[[j]] <- rbind(accuracy[[j]], tmpacc[[j]])
-    }
-  }
-  accuracy <- accuracy %>% map(function(x) sign(x$fe-x$te)) %>% rbind.data.frame()
-  colnames(accuracy) <- controls %>% map(function(x) paste0('depth = ',x[[2]]))
-  accuracy <- cbind(apply(accuracy, 2, function(x) sum(x == 1)),
-                    apply(accuracy, 2, function(x) sum(x == 0)),
-                    apply(accuracy, 2, function(x) sum(x == -1)))
-  colnames(accuracy) <- c('w', 'd', 'l')
-  accuracy
+
+  return(list(accuracyf, accuracymte))
 }
 
+
+dnames <- c('kick', 'upselling', 'internet', 'churn', 'appetency', 
+            'epsilon', 'click', 'amazon', 'adult')
 
 controls <- list(list(cp = 0, maxdepth = 2),
                  list(cp = 0, maxdepth = 4), list(cp = 0, maxdepth = 6))
-npts <- c(1000, 2000, 5000, 0)
 
 for(i in 1:length(npts)){
   res <- experiment(npts[i], controls)
